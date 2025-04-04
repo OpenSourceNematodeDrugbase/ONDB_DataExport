3e20aabd83c31aa45ff1d039330eb3174aaa93db
Ideasog01 <oliver.dev01@outlook.com>
2025-04-04 13:42:21 +0100

    Export 'lethal' entries in simplemine_results.csv

--- DataImport.py
+++ DataImport.py
@@ -1,5 +1,33 @@
 import pandas as pd
+import os
 
-df = pd.read_csv('mart_export.csv')
+def look_up_keyword(df, column_name, keyword):
+    results = df[df[column_name].str.contains(keyword, case=False, na=False)]
+    print(f"Results for lethal: {len(results)}")
+    return results
 
-print(df.head())
+def export_json(results, output_file_name):
+    # Gets the root directory of the script
+    root_directory = os.getcwd()
+    output_path = os.path.join(root_directory, output_file_name)
+
+    # Export results to JSON in root directory
+    results.to_json(output_path, orient="records", indent=4)
+
+    print(f"Results exported to {output_path}")
+
+
+mart_df = pd.read_csv('mart_export.txt')
+print(mart_df.head())
+
+sm_df = pd.read_csv("simplemine_results.txt", sep="\t")
+print(sm_df.head())
+
+lethalResults = look_up_keyword(sm_df, "RNAi Phenotype Observed", 'lethal')
+print(lethalResults)
+
+export_json(lethalResults, "lethalResults.json")
+
+
+
+
